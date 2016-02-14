f8d211c31275a8d78f8eb7459eb9b517afa27829
cbo <christophe_xxx@yahoo.fr>
2016-02-14 12:58:35 +0100

    Restore my site

--- data/SMAttributes.xml.php
+++ data/SMAttributes.xml.php
@@ -6,16 +6,23 @@
 	<entry key="SMGalleryWidth" value=""/>
 	<entry key="SMGalleryHeight" value="180"/>
 	<entry key="SMGalleryPadding" value="3"/>
-	<entry key="SMContactRecipients" value="mail@example-domain.com"/>
-	<entry key="SMContactSubject" value="Website Contact Form"/>
-	<entry key="SMContactSuccessMessage" value="Thanks, we'll get back to you shortly"/>
+	<entry key="SMContactRecipients" value="christophe_xxx@yahoo.fr"/>
+	<entry key="SMContactSubject" value="Location Gite"/>
+	<entry key="SMContactSuccessMessage" value="Merci de votre message, notre r&amp;eacute;ponse ne devrait pas tarder"/>
+	<entry key="SMPagesAutoSeoUrlsVersion" value="20140720"/>
+	<entry key="SMAutoSeoUrlsCompleted" value="true"/>
+	<entry key="SMPagesSettingsUrlType" value="Filename"/>
+	<entry key="SMPagesSettingsSeoUrls" value="true"/>
 	<entry key="SMImageMontageMinHeight" value="100"/>
 	<entry key="SMImageMontageMaxHeight" value="300"/>
 	<entry key="SMImageMontageMargin" value="0"/>
-	<entry key="SMImageMontageDisplayTitle" value="true"/>
+	<entry key="SMImageMontageDisplayTitle" value="false"/>
 	<entry key="SMImageMontageDisplayImageTitle" value="false"/>
 	<entry key="SMImageMontageDisplayImageExif" value="false"/>
 	<entry key="SMImageMontageDisplayPicker" value="false"/>
 	<entry key="SMImageMontageShuffle" value="true"/>
 	<entry key="SMImageMontageSlideShowInterval" value="3000"/>
+	<entry key="SMClientCacheKey" value="7"/>
+	<entry key="SMGoogleAnalyticsTrackerId" value="UA-67845419-1"/>
+	<entry key="SMGoogleAnalyticsReportMode" value="Everything"/>
 </database>
