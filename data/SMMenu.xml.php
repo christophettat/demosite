0c8f52e4e607abe3d953347a7181ad6809b5d31a
cbo <christophe_xxx@yahoo.fr>
2016-03-07 21:42:56 +0100

    typo (accents) and swimming pool

--- data/SMMenu.xml.php
+++ data/SMMenu.xml.php
@@ -1,8 +1,8 @@
 <?php exit(); ?>
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <database>
-	<entry id="b464ea5ac4e20d81bc307abb61c2ab62" parent="" order="9" title="La Region" url="index.php?SMExt=SMImageMontage&amp;amp;SMImageMontageDisplay=ImageMontage&amp;amp;SMImageMontageGallery=Photos"/>
+	<entry id="b464ea5ac4e20d81bc307abb61c2ab62" parent="" order="9" title="La R&amp;eacute;gion" url="index.php?SMExt=SMImageMontage&amp;amp;SMImageMontageDisplay=ImageMontage&amp;amp;SMImageMontageGallery=Photos"/>
 	<entry id="f0f099c49770fda2ee42b4a8b52d43b8" parent="" order="8" title="La Location" url="Tarifs.html"/>
 	<entry id="de7fa7c46dbde89f59a30bd134b16ae1" parent="" order="10" title="Contact" url="Contact.html"/>
-	<entry id="252eca08e2c8a8141df47cdb9c75f318" parent="" order="5" title="Le Gite" url="Index.html"/>
+	<entry id="252eca08e2c8a8141df47cdb9c75f318" parent="" order="5" title="Le G&amp;icirc;te" url="Index.html"/>
 </database>
