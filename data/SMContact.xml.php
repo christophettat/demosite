8d251dae88a5413fd74fc13cb1c5d97613675729
cbo <christophe_xxx@yahoo.fr>
2016-03-09 22:36:42 +0100

    typos om mama blij te houden

--- data/SMContact.xml.php
+++ data/SMContact.xml.php
@@ -3,6 +3,6 @@
 <database>
 	<entry id="8ff89f59524a305b944b057aea658492" title="Nom *" type="Textfield" position="1"/>
 	<entry id="f8fe77b10ebf8d9e88434b34a04e9fad" title="Message" type="Textbox" position="4"/>
-	<entry id="203d7907a68678186cc97fed082839d1" title="Telephone" type="Textfield" position="3"/>
+	<entry id="203d7907a68678186cc97fed082839d1" title="T&amp;eacute;l&amp;eacute;phone" type="Textfield" position="3"/>
 	<entry id="5081084c14f0fa0296cb22b35ca1abc9" title="e-mail *" type="Email" position="2"/>
 </database>
